9464751d94331a4b43eb5083587ae452d4592206
prba2567 <122951705+prba2567@users.noreply.github.com>
2023-03-23 15:37:04 -0600

    ASIGNMENT ((99999

--- wa/main2.js
+++ wa/main2.js
@@ -10,6 +10,7 @@ const overlay = document.querySelector('.overlay');
 const arr = ['chandler-walters-qFuKzUleXsw-unsplash.jpg', 'soheb-zaidi-qGereuMoZNw-unsplash.jpg', 'wolfgang-hasselmann-AKeb3CguZpw-unsplash.jpg','wolfgang-hasselmann-Ss_NBsZWIt8-unsplash.jpg','xx-liu-3qsrd-4-_pU-unsplash.jpg'];
 const alt = ['Pic 1', 'Pic 2', 'Pic 3', 'Pic 4', 'Pic 5'];
 
+console.log("I have loaded 2 ")
 function f(e)
 {
     console.log('HI')
@@ -19,9 +20,11 @@ displayedImage.alt = e.target.alt;
 }
 for(var i =0; i < arr.length; i++)
 {
+    console.log("I have loaded 3")
     var newImage = document.createElement('img');
     newImage.setAttribute('src', arr[i]);
-    newImage.setAttribute('alt', alt[i] );
+    newImage.setAttribute('alt', alt[i]);
+    console.log("I have loaded 4")
     thumbBar.appendChild(newImage);
     newImage.addEventListener('click',  e => {
         console.log("HI");
